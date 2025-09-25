8f93ce76095fc1b202ffd405300995a5c1517352
oleksandr piddubnyi <sanyakent2008@gmail.com>
2025-09-25 18:20:00 +0200

    hw6 110925

---
+++ project/funktion-tests/tests.py
@@ -0,0 +1,15 @@
+import pytest
+from funktion import get_ticket_price
+
+def test_child_free():
+    assert get_ticket_price(5) == 0.0
+
+def test_teen_discount():
+    assert get_ticket_price(10) == 50.0
+
+
+def test_adult_full_price():
+    assert get_ticket_price(30) == 100.0
+
+def test_senior_discount():
+    assert get_ticket_price(70) == 70.0
